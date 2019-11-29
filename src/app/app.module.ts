76f816721f4b6ee9509c993030b1fd9e36494eca
Harshali Kumbhar <harshali.kumbhar@zensar.com>
2019-11-29 12:53:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductManagementComponent } from './product-management/product-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductManagementComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,ProductManagementComponent]
 })
 export class AppModule { }
